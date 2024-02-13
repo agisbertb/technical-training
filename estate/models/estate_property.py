d2dad4096543dafdd8ef319cbefecba25b52e8a8
agisbertb <agisbert@iesebre.com>
2024-02-13 19:37:12 +0100

    Modificacions a la vista principal d'una propietat immobiliària

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -11,7 +11,7 @@ class EstateProperty(models.Model):
     expected_price = fields.Float('Preu Esperat')
     selling_price = fields.Float('Preu de Venda', readonly=True, copy=False)
     best_offer = fields.Float('Millor Oferta', compute='_compute_best_offer', readonly=True)
-    state = fields.Selection([('new', 'Nou'), ('offer_received', 'Oferta Rebut'), ('offer_accepted', 'Oferta Acceptada'), ('sold', 'Venut'), ('canceled', 'Cancel·lat')], string='Estat', default='new')
+    state = fields.Selection([('new', 'Nou'), ('offer_received', 'Oferta Rebuda'), ('offer_accepted', 'Oferta Acceptada'), ('sold', 'Venut'), ('canceled', 'Cancel·lat')], string='Estat', default='new')
     bedrooms = fields.Integer('Habitacions', required=True)
     lift = fields.Boolean('Ascensor', default=False)
     parking = fields.Boolean('Parking', default=False)
@@ -28,7 +28,7 @@ class EstateProperty(models.Model):
         ('E', 'E'),
         ('F', 'F'),
         ('G', 'G')
-    ], string='Certificat Energètic (Lletra)')
+    ], string='Certificat Energètic')
     active = fields.Boolean(default=True)
     offer_count = fields.Integer(compute='_compute_offer_count')
     buyer_id = fields.Many2one('res.partner', string='Comprador')
